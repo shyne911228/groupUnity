11c7474fb43832da86d05cd6ea0f08389b77eb4d
YiYunKung <grace519rm@gmail.com>
2022-07-29 14:53:19 +0800

    220729 Database

---
+++ Assets/Stuff/Inventroy/BagScripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/New Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemImage;
+    public int itemHeld;    //物品持有數量
+    [TextArea]
+    public string itemInform;
+
+    public bool equip;  //是否可裝備
+}
